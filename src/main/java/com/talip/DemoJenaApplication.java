88085fa5309b629c49e03826ecd698d2a32109ee
talip <talip.saritas@cogni.zone>
2020-02-21 15:37:34 +0100

    Initial commit

---
+++ src/main/java/com/talip/DemoJenaApplication.java
@@ -0,0 +1,13 @@
+package com.talip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJenaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talip/DemoJenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
